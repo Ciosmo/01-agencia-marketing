d277c2e33f1ee9d2621521ea30e128a18cacfce1
ciosmo <ig.navarroa@duocuc.cl>
2024-04-30 13:10:32 -0400

    Improving the frontend and making adjustments

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -3,10 +3,31 @@ import { NavLink, Link } from 'react-router-dom';
 import luffy from 'assets/img/logo.png';
 import star from 'assets/img/writing-loading.gif';
 
+
+
+
+
 function Navbar(){
+
+    window.onscroll  = function() {scrollFunction()}
+
+
+    function scrollFunction(){
+        if(document.getElementById('navbar')){
+            if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+                document.getElementById('navbar').classList.add('shadow-navbar');
+                document.getElementById('navbar').classList.add('bg-white');
+            }else{
+                document.getElementById('navbar').classList.remove('shadow-navbar');
+                document.getElementById('navbar').classList.remove('bg-white');
+            }
+        }
+    }
+
+
     return(
-        <nav className='w-full py-4 top-0 fixed'>
-            <div className="bg-white px-4 sm:px-6">
+        <nav id='navbar' className='w-full py-4 top-0 transition duration-300 ease-in-out z-40 fixed'>
+            <div className=" px-4 sm:px-6">
                 <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14 px-2">
                     <Link to='/' className="ml-4 mt-2">
                         <img 
@@ -17,15 +38,15 @@ function Navbar(){
                     </Link>
 
                     <div className="ml-4 mt-2 flex-shrink-0">
-                    <NavLink to='/casos' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Casos</NavLink>
-                    <NavLink to='/servicios' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Servicios</NavLink>
-                    <NavLink to='/nosotros' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Nosotros</NavLink>
-                    <NavLink to='/carreras' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Carreras</NavLink>
-                    <NavLink to='/blog' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Blog</NavLink>
-                    <NavLink to='/contacto' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Contacto</NavLink>
+                    <NavLink to='/casos' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-wine-btn-500 transition duration-300 ease-in-out mx-4">Casos</NavLink>
+                    <NavLink to='/servicios' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-wine-btn-500 transition duration-300 ease-in-out mx-4">Servicios</NavLink>
+                    <NavLink to='/nosotros' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-wine-btn-500 transition duration-300 ease-in-out mx-4">Nosotros</NavLink>
+                    <NavLink to='/carreras' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-wine-btn-500 transition duration-300 ease-in-out mx-4">Carreras</NavLink>
+                    <NavLink to='/blog' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-wine-btn-500 transition duration-300 ease-in-out mx-4">Blog</NavLink>
+                    <NavLink to='/contacto' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-wine-btn-500 transition duration-300 ease-in-out mx-4">Contacto</NavLink>
                         <button
                             type="button"
-                            className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-wine-btn px-6 py-2 text-l font-bold text-white shadow-sm transition duration-300 ease-in-out  hover:bg-yellowish-hover focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
+                            className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-wine-btn-500  px-6 py-2 text-l font-bold text-white shadow-sm transition duration-300 ease-in-out  hover:bg-yellowish-hover focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
                             
                             Hire us 
 
